87d5c2d3256e8256fa29f802696063cb4862fc99
Garrett Shortt <Gmshortt18@gmail.com>
2026-04-14 09:34:44 -0400

    We now have something resembling a working CLI.  It is held together by tape and chewing gum though, so some debugging and general refactoring will have to come next. After restructuring I allowed for the input to choose the file name and that is now the name of the file you read and what it saves the json file as.

--- flashcard/cli/main.py
+++ flashcard/cli/main.py
@@ -1,2 +1,12 @@
 import click 
-from 
+from flashcard.core.run import run
+
+@click.command()
+@click.option('--input', prompt='Which file should I run?')
+
+def main(input):
+    """Flashcard program that takes a CSV and creates a flashcard""" 
+    run(input)
+
+if __name__ == "__main__":
+    main()
